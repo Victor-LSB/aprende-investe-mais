095fa4b17e8e738a38d0cbbdf11277c98cb10494
Victor-LSB <victor.8298203@edu.univali.br>
2025-10-02 17:46:14 -0300

    a

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -12,10 +12,10 @@ const Progress = React.forwardRef<
     className={cn("relative h-4 w-full overflow-hidden rounded-full bg-secondary", className)}
     {...props}
   >
-    <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-accent transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
+<ProgressPrimitive.Indicator
+    className="h-full w-full flex-1 bg-accent transition-all" 
+  s tyle={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+/>
   </ProgressPrimitive.Root>
 ));
 Progress.displayName = ProgressPrimitive.Root.displayName;
